273e392531b37572b55a2e6cfa4fc52e5418759b
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-06-18 00:06:56 +0500

    added the Add Room fucntionaity

--- client/src/pages/hotelOwner/AddRoom.jsx
+++ client/src/pages/hotelOwner/AddRoom.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Title from '../../components/Title'
+import { assets } from '../../assets/assets'
 
 function AddRoom() {
+    const [images, setImages] = useState({
+        1:null,
+        2:null,
+        3:null,
+        4:null,
+    })
+    const [inputs , setInputs] = useState({
+        roomType:'',
+        pricePerNight:0,
+        amenities:{
+            'Free WiFi':false,
+            'Free Breakfast':false,
+            'Room Service':false,
+            'Mountain View':false,
+            'Pool Access':false
+        }
+    })
   return (
-    <div>AddRoom</div>
+    <form>
+        <Title align='left' font='outfit' title='Add Room' subTitle='Fill in the details carefully and accurate room details, pricing, and amenities, to enhance the user booking experience.'/>
+        {/* Upload Area for Images */}
+        <p className='text-gray-800 mt-10'>Images</p>
+        <div className='grid grid-cols-2 sm:flex gap-4 my-2 flex-wrap'>
+            {Object.keys(images).map((key)=>(
+                <label htmlFor={`roomImage${key}`} key={key}>
+                    <img className='max-h-13 cursor-pointer opacity-80'
+                    src={images[key] ? URL.createObjectURL(images[key]) : assets.uploadArea} alt=''/>
+                    <input type='file' accept='image/*' id={`roomImage${key}`} hidden onChange={e=>setImages({...images, [key]:e.target.files[0]})}/>
+                </label>
+            ))}
+        </div>
+        <div className='w-full flex max-sm:flex-col sm:gap-4 mt-4'> 
+            <div className='flex-1 max-w-48'>
+                <p className='text-gray-800 mt-4'>Room Type</p>
+                <select value={inputs.roomType} onChange={e=>setInputs({...inputs, roomType:e.target.value})} className='border opacity-70 border-gray-300 mt-1 rounded p-2 w-full'>
+
+                    <option value="">Select Room Type</option>
+                    <option value="Single Bed">Single Bed</option>
+                    <option value="Double Bed">Double Bed</option>
+                    <option value="Luxury Room">Luxury Room</option>
+                    <option value="Family Suite">Family Suite</option>
+                </select>
+
+            </div>
+        </div>
+    </form>
   )
 }
 
